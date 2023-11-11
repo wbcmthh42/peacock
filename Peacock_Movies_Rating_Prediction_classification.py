6e32b245492febe1a01fc68ffd862f4a30adfdda
Johnny <tayhhjohnny@outlook.com>
2023-11-11 15:40:19 +0800

    updates

--- Peacock_Movies_Rating_Prediction_classification.py
+++ Peacock_Movies_Rating_Prediction_classification.py
@@ -4,6 +4,7 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 import pickle
 import warnings
 import streamlit as st
@@ -11,31 +12,65 @@ from category_encoders import LeaveOneOutEncoder
 from sklearn.preprocessing import StandardScaler
 
 nltk.download('punkt')
-nltk.download('wordnet')
 lemmatizer = WordNetLemmatizer()
 warnings.filterwarnings('ignore')
+all_stopwords = stopwords.words("english")
 
 class data_prep():
     def __init__(self, datafile):
         self.data = datafile
-        self.data = self.data.rename(columns = {'director':'director_name'})
         self.data[['startYear','runtimeMinutes']] = self.data[['startYear','runtimeMinutes']].astype(int)
     
+    def genres_encoding(self):
+        """
+        Encodes the 'genres' column in the movie data.
+
+        Returns
+        -------
+        None
+        """
+        def gen_enc(data):
+            # Tokenize the 'genres' column and create a list of tokens
+            tokens = ' '.join(data['genres']).lower()
+            tokens = word_tokenize(tokens)
+            tokens = [token for token in tokens if any(char.isalpha() for char in token)]
+
+            # Remove duplicate tokens
+            unique_tokens = list(set(tokens))
+            
+            # Create a DataFrame to store the encoded genres
+            encoded_genres = pd.DataFrame()
+
+            # Iterate through each unique token
+            for token in unique_tokens:
+                # Create a new column for the encoded token
+                encoded_genres['type'+token] = data['genres'].apply(lambda x: int(token in x.lower()))
+
+            # Print the encoded genres
+            data = pd.concat([data.drop('genres',axis=1), encoded_genres], axis=1)
+            return data
+        
+        # Save the X_train columns as a pkl file
+        with open('X_train_columns.pkl', 'rb') as f:
+            genres_columns = pickle.load(f)
+        
+        self.data = gen_enc(self.data).reindex(columns=genres_columns, fill_value=0)
+
     def text_corpus_column(self):
-        self.data['corpus'] = self.data['primaryTitle'] + ' ' + self.data['genres'] + ' ' + self.data['narrative']
-        self.data = self.data.drop(['primaryTitle','narrative', 'genres'],axis=1)      
+        self.data['corpus'] = self.data['primaryTitle'] + ' ' + self.data['narrative']
+        self.data = self.data.drop(['tconst','primaryTitle','narrative', 'numVotes'],axis=1)     
 
     def custom_preprocessor(self, text):
         text = re.sub(r"<br\s*/?>\s*<br\s*/?>", " ", text)
         text = re.sub(r"[^\s\w]|_", " ", text)
         text = re.sub(r'[0-9]+', '', text)
+        text= text.split()
+        text = [lemmatizer.lemmatize(word) for word in text if not word in set(all_stopwords)]
+        text = ' '.join(text)
         return text.lower()
 
-    def custom_tokenizer(self, string_data):
-        tokens = word_tokenize(string_data)
-        return [lemmatizer.lemmatize(token, pos="v") for token in tokens]
-
     def tfidf_vectorizer(self):
+
         with open('tfidf_vectorizer.pkl', 'rb') as f:
             vectorizer = pickle.load(f)
         self.tfidf_new = vectorizer.transform(self.data['corpus'])
@@ -44,13 +79,9 @@ class data_prep():
     
     def combine_features(self):
         self.data = pd.concat([self.data.drop('corpus',axis=1), self.tfidf_new], axis = 1)
-        self.data_cat = self.data.select_dtypes(exclude=np.number)
+        self.data_cat = self.data.select_dtypes(exclude=np.number).apply(lambda x: x.str.lower())
         self.data_num = self.data.select_dtypes(include=np.number)
-  
-    def scale_num_cols(self):
-        scaler = StandardScaler()
-        self.data_num = pd.DataFrame(scaler.fit_transform(self.data_num))
-    
+
     def ohe_cat_cols(self):
         with open('leave_one_out_vectorizer.pkl', 'rb') as f:
             categorical_pipeline = pickle.load(f)
@@ -59,24 +90,25 @@ class data_prep():
         self.data = pd.concat([self.data_num.reset_index(),self.data_cat_encoded.reset_index()], axis=1)
         return self.data
 
+    def printer(self):
+        display(self.data)
+
 class Model(data_prep):
     def __init__(self, datafile):
         super().__init__(datafile)
+        super().genres_encoding()
         super().text_corpus_column()
         super().tfidf_vectorizer()
         super().combine_features()
         super().ohe_cat_cols()
         self.data = self.data.drop('index',axis=1)
-        print(self.data)
-        with open('svd_vectorizer2.pkl', 'rb') as f:
-            svd = pickle.load(f)
-        self.data = svd.transform(self.data)
 
     def load_model(self):
-        with open('Completed_rfc500.pkl', 'rb') as f:
+        with open('rfc.pkl', 'rb') as f:
             model = pickle.load(f)
         predictions = model.predict(self.data)
         return predictions
+
 
 if __name__ == '__main__':
     st.title('Peacock Film Finance')
@@ -86,7 +118,7 @@ if __name__ == '__main__':
     runtimeMinutes = st.text_input('Runtime (in minutes)', placeholder='Enter movie runtime in minutes...')
     genres = st.text_input('Genres', placeholder='Enter movie genre...')
     narrative = st.text_input('Plot Summary', placeholder='Enter a brief narrative of the movie plot...')
-    director = st.text_input('Director', placeholder='Enter name of director...')
+    director_name = st.text_input('Director', placeholder='Enter name of director...')
     lead_actor_or_actress = st.text_input('Lead Actor/Actress', placeholder='Enter name of lead actor or actress...')
 
     data = {'primaryTitle': [primaryTitle],
@@ -94,7 +126,7 @@ if __name__ == '__main__':
             'runtimeMinutes': [runtimeMinutes],
             'genres': [genres],
             'narrative': [narrative],
-            'director': [director],
+            'director_name': [director_name],
             'lead_actor_or_actress': [lead_actor_or_actress]}
 
     df = pd.DataFrame(data)
@@ -105,4 +137,3 @@ if __name__ == '__main__':
             st.markdown('The predicted rating is :red[not expected] to do well with rating at :red[lesser than 7 out of 10]')
         else:
             st.markdown('The predicted rating is :green[expected] to do well with rating at :green[7 or above out of 10]')
-
